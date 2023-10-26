a285a6b78956f729b44629c329aec4d8095d4923
kasib <kasibzxc@gmail.com>
2023-10-26 15:56:38 -0400

    initial commit with inheritance Try Yourself

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import Inheritance.Cat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Inheritance
+        var myCat = new Cat("Gypsy");
+        System.out.println(myCat.toString());
+
+    }
+}
